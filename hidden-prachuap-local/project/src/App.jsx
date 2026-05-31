31246fd49fbed987c0f1508f77352a13f27b08c6
Jyrelle xD <jyrellexd@gmail.com>
2026-06-01 03:38:38 +0800

    add scroll to top on navigation

--- hidden-prachuap-local/project/src/App.jsx
+++ hidden-prachuap-local/project/src/App.jsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
+import ScrollToTop from '@/components/ScrollToTop';
 
 import Layout from '@/components/Layout';
 import Home from '@/pages/Home';
@@ -14,6 +15,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClientInstance}>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
